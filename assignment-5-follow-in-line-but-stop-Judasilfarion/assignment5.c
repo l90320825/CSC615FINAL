8cb685f3f9f9ee85b19965c8393a55b0b4ec31b5
Judasilfarion <rau3@mail.sfsu.edu>
2020-12-04 01:55:31 +0000

    Wheels turn when Line Sensor goes off

--- assignment-5-follow-in-line-but-stop-Judasilfarion/assignment5.c
+++ assignment-5-follow-in-line-but-stop-Judasilfarion/assignment5.c
@@ -1,38 +1,58 @@
-/**************************************************************
-* Class: CSC-615-01 Spring 2020
-* Name: Raymond Au
-* Student ID: 916672216
-* Github ID: Judasilfarion
-* Project: Assignment 5 Follow In Line But Stop
-*
-* File: assignment5.c
-*
-* Description: Assignment 5 code for IR obstacle sensor and line detector
-*
+/************************************************************** * Class: CSC-615-01 Spring 2020 * Name: Raymond 
+Au * Student ID: 916672216 * Github ID: Judasilfarion * Project: Assignment 5 Follow In Line But Stop * * File: 
+assignment5.c * * Description: Assignment 5 code for IR obstacle sensor and line detector * 
 **************************************************************/
 
 #include <stdio.h>
 #include <wiringPi.h>
 #include <pthread.h>
 #include <time.h>
+#include <softPwm.h>
 
-#define IR 24
-#define LINE 0
+#define LINE 25
+#define TRIG1 0
+#define TRIG2 6
+#define TRIG3 12
+#define TRIG4 26
+#define FORWARD1 3
+#define FORWARD2 5
+#define FORWARD3 13
+#define FORWARD4 11
 
 int exitbool = 0;
 int lineDetect = 0;
-int IRDetect = 0;
+int pwm;
+int vpwm = 0;
 
 void *lineThread(void *vargp) {
 	while (exitbool == 0) {
 		lineDetect = digitalRead(LINE);
+		if (lineDetect == 0) {
+			pwm = 100;
+		}
+		else pwm = 0;
 	}
 	return NULL;
 }
 
-void *IRThread(void *vargp) {
+void *wheelThread(void *vargp) {
 	while (exitbool == 0) {
-		IRDetect = digitalRead(IR);
+		if (vpwm < pwm) {
+			for (vpwm; vpwm < pwm; vpwm++) {
+				softPwmWrite (FORWARD1, vpwm);
+				softPwmWrite (FORWARD2, vpwm);
+				softPwmWrite (FORWARD3, vpwm);
+				softPwmWrite (FORWARD4, vpwm);
+			}
+		}
+		if (vpwm > pwm) {
+			for (vpwm; vpwm > pwm; vpwm--) {
+				softPwmWrite (FORWARD1, vpwm);
+				softPwmWrite (FORWARD2, vpwm);
+				softPwmWrite (FORWARD3, vpwm);
+				softPwmWrite (FORWARD4, vpwm);
+			}
+		}
 	}
 	return NULL;
 }
@@ -47,21 +67,34 @@ if (wiringPiSetup () == -1){
 printf("Setup successful\n");
 fflush(stdout);
 
-pthread_t linethread_id, irthread_id, inputthread_id;
-pinMode (IR, INPUT);
+pinMode (TRIG1, 1);
+pinMode (TRIG2, 1);
+pinMode (TRIG3, 1);
+pinMode (TRIG4, 1);
+digitalWrite (TRIG1, 1);
+digitalWrite (TRIG2, 1);
+digitalWrite (TRIG3, 1);
+digitalWrite (TRIG4, 1);
+
+softPwmCreate (FORWARD1, 0, 100);
+softPwmCreate (FORWARD2, 0, 100);
+softPwmCreate (FORWARD3, 0, 100);
+softPwmCreate (FORWARD4, 0, 100);
+
+pthread_t linethread_id, wheelthread_id;
 pinMode (LINE, INPUT);
 
 printf("Creating threads for sensors \n");
 fflush(stdout);
 
 pthread_create(&linethread_id, NULL, lineThread, NULL);
-pthread_create(&irthread_id, NULL, IRThread, NULL);
+pthread_create(&wheelthread_id, NULL, wheelThread, NULL);
 
 time_t seconds = time(NULL);
 while (time(NULL) < seconds + 30) {
 	printf("Line Sensor: %d\n", lineDetect);
 	fflush(stdout);
-	printf("IR Sensor: %d\n", IRDetect);
+	printf("PWM: %d\n", pwm);
 	fflush(stdout);
 	delay(1000);
 }
