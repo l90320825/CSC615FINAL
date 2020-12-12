753dd755a07d129cc9c3172afbb0a9bc6b6d6f65
Judasilfarion <damnedemailthing@gmail.com>
2020-12-11 18:39:39 -0800

    Added comments to code

--- assignment-5-follow-in-line-but-stop-Judasilfarion/assignment5.c
+++ assignment-5-follow-in-line-but-stop-Judasilfarion/assignment5.c
@@ -9,39 +9,55 @@ assignment5.c * * Description: Assignment 5 code for IR obstacle sensor and line
 #include <time.h>
 #include <softPwm.h>
 
+// GPIO pin for the line sensor
 #define LINE 25
+
+// GPIO pins for turning on the motors, probably don't need to touch these
 #define TRIG1 0
 #define TRIG2 6
 #define TRIG3 12
 #define TRIG4 26
-#define FORWARD1 2
-#define FORWARD2 4
-#define FORWARD3 14
-#define FORWARD4 10
-#define REVERSE1 3
-#define REVERSE2 5
-#define REVERSE3 13
-#define REVERSE4 11
 
+// GPIO pins for power output to the motors, used to make them spin
+#define FORWARD1 2 	// Right rear wheel
+#define FORWARD2 4 	// Left rear wheel
+#define FORWARD3 14 	// Left front wheel
+#define FORWARD4 10 	// Right front wheel
+#define REVERSE1 3 	// Right rear wheel
+#define REVERSE2 5 	// Left rear wheel
+#define REVERSE3 13 	// Left front wheel
+#define REVERSE4 11 	// Right front wheel
+
+// Used to determine whether or not the program should shut down all threads and exit
 int exitbool = 0;
+
+// If this is 1, then the car detects the line. If this is 0, then the car does not detect the line
 int lineDetect = 0;
+
+// The target pwm of the motors
 int pwm;
+
+// The current pwm of the motors
 int vpwm = 0;
 
+// This thread runs a while loop that continually reads input from the line sensor in order to determine whether or not sensors detect a line.
+// It currently assumes that the car only has a single line sensor - The sensor attached directly to the front of the car. Support for the other line sensors will be implemented later.
 void *lineThread(void *vargp) {
 	while (exitbool == 0) {
-		lineDetect = digitalRead(LINE);
+		lineDetect = digitalRead(LINE); // Read the input from the line sensor
 		if (lineDetect == 1) {
-			pwm = 50;
+			pwm = 50; // pwm = 50 sets the target pwm of the motors to half speed. 100 is currently way too fast for testing purposes.
 		}
 		else pwm = 0;
 	}
 	return NULL;
 }
 
+// This thread runs the while loop that modifies the pwm of the motors so that they can start and stop.
 void *wheelThread(void *vargp) {
 	while (exitbool == 0) {
 		if (vpwm < pwm) {
+			// If current pwm is less than target pwm, run a loop that sets power output to all motors to max (max is currently defined by pwm = 50 in the lineThread), which sets motor speed to max.
 			for (vpwm; vpwm < pwm; vpwm++) {
 				softPwmWrite (FORWARD1, vpwm);
 				softPwmWrite (FORWARD2, vpwm);
@@ -50,6 +66,7 @@ void *wheelThread(void *vargp) {
 			}
 		}
 		if (vpwm > pwm) {
+			// If current pwm is greater than target pwm, run a loop that sets power output to all motors to 0, which stops all wheels.
 			for (vpwm; vpwm > pwm; vpwm--) {
 				softPwmWrite (FORWARD1, vpwm);
 				softPwmWrite (FORWARD2, vpwm);
@@ -61,7 +78,10 @@ void *wheelThread(void *vargp) {
 	return NULL;
 }
 
+// Main function
 int main (void) {
+
+// Setup wiringpi
 if (wiringPiSetup () == -1){
 	printf("Setup failed\n");
 	fflush(stdout);
@@ -71,6 +91,7 @@ if (wiringPiSetup () == -1){
 printf("Setup successful\n");
 fflush(stdout);
 
+// Turn on motors
 pinMode (TRIG1, 1);
 pinMode (TRIG2, 1);
 pinMode (TRIG3, 1);
@@ -80,6 +101,7 @@ digitalWrite (TRIG2, 1);
 digitalWrite (TRIG3, 1);
 digitalWrite (TRIG4, 1);
 
+// Create pwm for motors
 softPwmCreate (FORWARD1, 0, 100);
 softPwmCreate (FORWARD2, 0, 100);
 softPwmCreate (FORWARD3, 0, 100);
@@ -89,6 +111,7 @@ softPwmCreate (REVERSE2, 0, 100);
 softPwmCreate (REVERSE3, 0, 100);
 softPwmCreate (REVERSE4, 0, 100);
 
+// Create threads for line sensor and motors
 pthread_t linethread_id, wheelthread_id;
 pinMode (LINE, INPUT);
 
@@ -98,6 +121,7 @@ fflush(stdout);
 pthread_create(&linethread_id, NULL, lineThread, NULL);
 pthread_create(&wheelthread_id, NULL, wheelThread, NULL);
 
+// Runs the program for 5 minutes before shutting down
 time_t seconds = time(NULL);
 while (time(NULL) < seconds + 300) {
 	printf("Line Sensor: %d\n", lineDetect);
